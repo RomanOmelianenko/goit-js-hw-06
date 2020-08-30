6af0542a0748fe9d4b9c2d0d31bc3bbc49d57e0e
RomanOmelianenko <r_omelianenko@ukr.net>
2020-08-31 00:08:46 +0300

    Homework-6

---
+++ task-9.js
@@ -0,0 +1,13 @@
+import users from "./users.js";
+
+// Массив имен(поле name) людей, отсортированных в зависимости от количества их друзей(поле friends)
+
+const getNamesSortedByFriendsCount = (users) => {
+  // твой код
+  return users
+    .sort((a, b) => a.friends.length - b.friends.length)
+    .map((user) => user.name);
+};
+
+console.table(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
